7627e8fffc8bb60bcfcae1e1e513c1c74d802491
Marco Bertolaccini <contacto@marco.ar>
2024-07-26 19:30:05 -0300

    Changed source path

--- git.zsh
+++ git.zsh
@@ -2,6 +2,6 @@
 # git VSC info
 ###############################
 
-source ./git/git-prompt.sh
+source ~/.zsh/git/git-prompt.sh
 #precmd () { __git_ps1 "%n" ":%~$ " "|%s" }
 setopt PROMPT_SUBST ; PS1='[%n@%m %c$(__git_ps1 " (%s)")]\$ '
