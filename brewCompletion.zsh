c892ad44cf87c7e1abe4e1f9429e095aaf8f1574
Marco Bertolaccini <contacto@marco.ar>
2024-08-08 22:30:54 -0300

    Added talosctl cli site-functions path

--- brewCompletion.zsh
+++ brewCompletion.zsh
@@ -4,4 +4,11 @@
 
 fpath=(/opt/homebrew/share/zsh/site-functions $fpath)
 
+###############################
+# talosctl. Add site-functions in $FPATH
+# talosctl installation did not create site-functions symlink in /opt/homebrew/share/zsh/site-functions/ folder.
+###############################
+
+fpath=(/opt/homebrew/opt/talosctl/share/zsh/site-functions $fpath)
+
 autoload -Uz compinit && compinit
