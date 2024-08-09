767f71bcf492d6051a506548ae5ff9f453275ceb
Marco Bertolaccini <contacto@marco.ar>
2024-08-09 00:59:48 -0300

    Added docker lima context env

--- env.zsh
+++ env.zsh
@@ -24,3 +24,15 @@ path=('/Applications/Visual Studio Code.app/Contents/Resources/app/bin' $path)
 
 # Export to sub-processes (make it inherited by child processes)
 export PATH
+
+
+###############################
+# Export docker lima context if the VM already exists
+###############################
+
+LIMA_INSTANCE=docker
+
+if [[ "$(limactl ls -q "$LIMA_INSTANCE" 2>/dev/null)" == "$LIMA_INSTANCE" ]]; then
+  DOCKER_HOST=$(limactl list "$LIMA_INSTANCE" --format 'unix://{{.Dir}}/sock/docker.sock')
+  export DOCKER_HOST
+fi
